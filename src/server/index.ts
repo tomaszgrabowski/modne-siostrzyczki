10c079512ad1245416ba341c29cdd58f11f959f3
Tomasz Grabowski <tomasz.grabowski@ihsmarkit.com>
2018-12-01 12:52:51 +0100

    node + cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as jwt from "jsonwebtoken";
 import { MongoClient } from "mongodb";
+import * as cors from 'cors';
 
 export class Server {
   private port: string | number;
@@ -19,6 +20,8 @@ export class Server {
   run(): void {
     this.app.use(express.json());
     this.app.use(express.urlencoded());
+    this.app.use(cors());
+    
     MongoClient.connect(
       "mongodb://localhost:27017",
       (err, client) => {
@@ -41,6 +44,7 @@ export class Server {
             }
           );
 
+          //LOGIN
           this.app.post(
             "/login",
             (req: express.Request, res: express.Response) => {
@@ -57,6 +61,34 @@ export class Server {
                     res.sendStatus(403);
                   }
                 });
+            }
+          );
+
+          //PRODUCTS
+          this.app.get(
+            "/products",
+            (req: express.Request, res: express.Response) => {
+              db.collection("products")
+                .find({})
+                .toArray()
+                .then(products => {
+                  res.status(200).json(products);
+                });
+            }
+          );
+          this.app.post(
+            "/products",
+            this.verifyToken,
+            (req: express.Request, res: express.Response) => {
+              jwt.verify(req["token"], this.salt, (err, authData) => {
+                if (err) {
+                  res.sendStatus(403);
+                } else {
+                  db.collection("products").insert(req.body).then(()=>{
+                    res.sendStatus(201);
+                  });
+                }
+              });
             }
           );
         });
