35d1888d218e75f5ab84cc188b4938c370ef40a0
Tomasz Grabowski <tomasz.grabowski@ihsmarkit.com>
2018-12-04 15:11:23 +0100

    switching to manually add img

--- src/app/directives/product-thumbnail.directive.ts
+++ src/app/directives/product-thumbnail.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, Input, OnInit, HostBinding } from "@angular/core";
 import { ProductImage } from "src/models";
+import { ConstantsService } from "../services/constants.service";
 
 @Directive({
   selector: "[app-product-thumbnail]"
@@ -11,11 +12,15 @@ export class ProductThumbnailDirective implements OnInit {
   @HostBinding("attr.src")
   src;
 
-  constructor() {}
+  constructor(
+    private constants: ConstantsService
+  ) {}
 
   ngOnInit(): void {
     if (this.items) {
-      this.src = this.items.find(item => item.thumbnail).url || this.items[0];
+      const prefix =  this.constants.DEV_MODE ? `http://localhost:1334/photos/` : `/photos/`;
+      const image = this.items.find(item => item.thumbnail).url || this.items[0].url;
+      this.src = `${prefix}${image}`;
     }
   }
 }
